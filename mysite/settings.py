043384403edd943c8d382438e6e4bfbbf490876f
miarevaloto <juanmi.arevaloto@ecci.edu.co>
2026-06-22 12:56:13 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
 
 # ================= MIDDLEWARE =================
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
